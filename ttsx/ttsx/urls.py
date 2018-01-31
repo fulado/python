42222b9109c87a932a1a6510c2108c40a698b140
barry <lichunbai@foxmail.com>
2018-01-31 20:16:43 +0800

    modify url

--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('app_ttsx.urls')),
+    url(r'^user/', include('app_ttsx.urls')),
 ]
