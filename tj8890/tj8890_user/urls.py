3952cd6fe9115cc6fce755ccaad880242a342bd0
barry <lichunbai@foxmail.com>
2018-03-22 17:49:31 +0800

    20130322

--- tj8890/tj8890_user/urls.py
+++ tj8890/tj8890_user/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.login),
+    url(r'^$', views.login),
+    url(r'^login/$', views.login_service),
 ]
 
 
--- tj8890/tj8890_user/views.py
+++ tj8890/tj8890_user/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import User
+import hashlib
 
 # Create your views here.
 
@@ -6,3 +8,32 @@ from django.shortcuts import render
 # 显示登录页
 def login(request):
     return render(request, 'user/login.html')
+
+
+# 登录服务
+def login_service(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    # 根据用户名查询用户
+    user_list = User.objects.filter(username=username)
+    # 如果用户不存在返回错误
+    if len(user_list) > 0:
+        # 如果用户存在, 对比密码
+        # 对密码进行sha1加密
+        password_encrypted = hashlib.sha1(password.encode('utf-8')).hexdigest()
+        user = user_list[0]
+
+        if password_encrypted == user.password:
+            error = False
+        else:
+            # 密码不同返回错误
+            error = True
+    else:
+        error = True
+    print(error)
+    if error:
+        context = {'username': username, 'password': password, 'error': error}
+        return render(request, 'user/login.html', context)
+    else:
+        return render(request, 'user/success.html')
