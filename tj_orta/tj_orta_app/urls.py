8a19774647ca2818bcce3a7c9b69ad58c6f04011
fulado <lichunbai@outlook.com>
2018-04-23 00:29:34 +0800

    enterprise compelete

--- tj_orta/tj_orta_app/urls.py
+++ tj_orta/tj_orta_app/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     # url(r'^create_admin/', views.create_admin),  # 创建管理员帐号, 不要轻易使用
     url(r'^main/?', views.main),
     url(r'^enterprise_add/?', views.enterprise_add),
+    url(r'^enterprise_modify/?', views.enterprise_modify),
+    url(r'^enterprise_delete/?', views.enterprise_delete),
     url(r'^enterprise/?', views.enterprise),
+    url(r'^vehicle/?', views.vehicle),
+    url(r'^verify/?', views.verify),
+    url(r'^is_user_exist/?', views.is_user_exist),
 ]
-
-
--- tj_orta/tj_orta_app/views.py
+++ tj_orta/tj_orta_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
+from django.http import JsonResponse
 from .models import User
+from tj_orta.utils import MyPaginator
 import hashlib
 
 # Create your views here.
@@ -14,9 +16,22 @@ def main(request):
 # 显示企业管理页面
 def enterprise(request):
     # 查询企业信息
-    enterprise_list = User.objects.filter(authority_id=1).filter(is_delete=False)
-    
-    context = {'enterprise_list': enterprise_list}
+    enterprise_list = User.objects.filter(authority_id=1).filter(is_delete=False).order_by('-id')
+
+    # 获取企业搜索信息
+    search_name = request.GET.get('search_name', '')
+    # 在结果集中搜索包含搜索信息的企业
+    if search_name != '':
+        enterprise_list = enterprise_list.filter(enterprise_name__contains=search_name)
+
+    # 获得用户指定的页面
+    page_num = int(request.GET.get('page_num', 1))
+
+    # 创建分页
+    mp = MyPaginator()
+    mp.paginate(enterprise_list, 10, page_num)
+    print(search_name)
+    context = {'mp': mp, 'search_name': search_name}
     
     return render(request, 'enterprise.html', context)
 
@@ -25,7 +40,7 @@ def enterprise(request):
 def enterprise_add(request):
     # 获取企业信息
     username = request.POST.get('username')                 # 帐号
-    password = request.POST.get('password')                 # 密码
+    password = request.POST.get('password')                 # 密码 不能使用'12345678'
     authority = int(request.POST.get('author'))             # 权限等级
     enterprise_name = request.POST.get('companyName', '')   # 企业名称
     enterprise_phone = request.POST.get('tel', '')          # 企业联系方式
@@ -53,3 +68,83 @@ def enterprise_add(request):
         print(e)
 
     return HttpResponseRedirect('/enterprise')
+
+
+# 判断用户名是否已经存在
+def is_user_exist(request):
+    username = request.GET.get('username')
+    user_id = request.GET.get('id', 0)
+
+    if user_id == 0:
+        is_exist = User.objects.filter(username=username).exists()
+    else:
+        is_exist = User.objects.filter(username=username).exclude(id=user_id).exists()
+
+    return JsonResponse({'is_exist': is_exist})
+
+
+# 编辑企业信息
+def enterprise_modify(request):
+    # 获取企业信息
+    username = request.POST.get('username')                 # 帐号
+    password = request.POST.get('password')                 # 密码
+    authority = int(request.POST.get('author'))             # 权限等级
+    enterprise_name = request.POST.get('companyName', '')   # 企业名称
+    enterprise_phone = request.POST.get('tel', '')          # 企业联系方式
+    legal_person = request.POST.get('corName', '')          # 法人
+    organization_code = request.POST.get('orgCode', '')     # 组织机构代码
+    contact = request.POST.get('contect', '')               # 联系人
+    contact_phone = request.POST.get('conMobile', '')       # 联系人电话
+    user_id = request.POST.get('id')                        # 用户id
+
+    # 查询用户
+    user = User.objects.filter(id=user_id).filter(is_delete=False)[0]
+
+    # 保存user信息
+    user.username = username
+    user.authority_id = authority
+    user.enterprise_name = enterprise_name
+    user.enterprise_phone = enterprise_phone
+    user.legal_person = legal_person
+    user.organization_code = organization_code
+    user.contact = contact
+    user.contact_phone = contact_phone
+    # 如果密码不能8个'#', 需要修改密码
+    if password != r'########':
+        user.password = hashlib.sha1(password.encode('utf8')).hexdigest()
+        print('change password')
+
+    # 存入数据库
+    try:
+        user.save()
+    except Exception as e:
+        print(e)
+
+    return HttpResponseRedirect('/enterprise')
+
+
+# 删除用户信息
+def enterprise_delete(request):
+    # 获取用户id
+    user_id = request.POST.get('id')  # 用户id
+
+    # 查询user
+    user = User.objects.filter(id=user_id).filter(is_delete=False)[0]
+
+    # 删除user
+    try:
+        user.delete()
+    except Exception as e:
+        print(e)
+
+    return HttpResponseRedirect('/enterprise')
+
+
+# 显示车辆管理页面
+def vehicle(request):
+    return render(request, 'vehicle.html')
+
+
+# 显示审核页面
+def verify(request):
+    return render(request, 'verify.html')
