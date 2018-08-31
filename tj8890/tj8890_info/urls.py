b6b6af94369146de4f18775d6f742255aa3e11c8
barry <lichunbai@outlook.com>
2018-08-31 18:13:42 +0800

    201808310613

--- tj8890/tj8890_info/urls.py
+++ tj8890/tj8890_info/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^all_show/?', views.all_show),
     url(r'^detail_show/?', views.detail_show),
+    url(r'^save_info/?', views.save_info),
+    url(r'^delete_info/?', views.delete_info),
 ]
 
--- tj8890/tj8890_info/views.py
+++ tj8890/tj8890_info/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from tj8890_item.models import Category
 from .models import Information
+from tj8890.utils import MyPaginator
 
 import time
 
@@ -9,6 +10,8 @@ import time
 
 # 显示所有知识页面
 def all_show(request):
+    title = ['知识库管理', '内容查询']
+
     # 获得查询知识提交的信息
     cate1 = int(request.GET.get('cate1', 0))
     cate2 = int(request.GET.get('cate2', 0))
@@ -22,6 +25,8 @@ def all_show(request):
         recd_time_begin = time.strftime('%Y-01-01', time.localtime())
     if recd_time_end == '0':
         recd_time_end = time.strftime('%Y-%m-%d', time.localtime())
+
+    recd_time_end_research = '%s 23:59:59' % recd_time_end
 
     # 将检索信息保存到session
     request.session['cate1'] = cate1
@@ -42,8 +47,8 @@ def all_show(request):
     info_list = Information.objects.all().order_by('-upload_time')
 
     # 按照表单中的的信息进行过滤
-    info_list = info_list.filter(recd_time__gte=recd_time_begin)
-    info_list = info_list.filter(recd_time__lte=recd_time_end)
+    info_list = info_list.filter(upload_time__gte=recd_time_begin)
+    info_list = info_list.filter(upload_time__lte=recd_time_end_research)
 
     if cate1 != 0:
         info_list = info_list.filter(category1_id=cate1)
@@ -54,9 +59,90 @@ def all_show(request):
     if cate4 != 0:
         info_list = info_list.filter(category4_id=cate4)
 
-    return render(request, 'info/all.html')
+    # 获得用户指定的页面
+    page_num = int(request.GET.get('page_num', 1))
+    # 分页
+    mp = MyPaginator()
+    mp.paginate(info_list, 10, page_num)
+
+    context = {'mp': mp,
+               'cate1_list': cate1_list,
+               'cate2_list': cate2_list,
+               'cate3_list': cate3_list,
+               'cate4_list': cate4_list,
+               'title': title,
+               }
+
+    return render(request, 'info/all.html', context)
 
 
 # 显示知识详情页面
 def detail_show(request):
-    return render(request, 'info/detail.html')
+    title = ['知识库管理', '添加内容']
+
+    info_id = int(request.GET.get('id', 0))
+    can_save = int(request.GET.get('add', 0))
+    can_delete = int(request.GET.get('delete', 0))
+
+    if info_id == 0:
+        info_id = 1
+
+    info = Information.objects.get(id=info_id)
+
+    # 事项分类
+    cate1_list = Category.objects.filter(level=1)
+    cate2_list = Category.objects.filter(level=2)
+    cate3_list = Category.objects.filter(cate_id=0)
+    cate4_list = Category.objects.filter(cate_id=0)
+
+    context = {'info': info,
+               'can_save': can_save,
+               'can_delete': can_delete,
+               'cate1_list': cate1_list,
+               'cate2_list': cate2_list,
+               'cate3_list': cate3_list,
+               'cate4_list': cate4_list,
+               'title': title,
+               }
+
+    return render(request, 'info/detail.html', context)
+
+
+# 保存知识
+def save_info(request):
+    info_name = request.GET.get('info_name', '')
+    info_abstract = request.GET.get('info_abstract', '')
+    info_content = request.GET.get('info_content', '')
+    # cate1 = int(request.GET.get('cate1', ''))
+    # cate2 = int(request.GET.get('cate2', ''))
+    # cate3 = int(request.GET.get('cate3', ''))
+    # cate4 = int(request.GET.get('cate4', ''))
+
+    user_id = request.session.get('user_id', 1)
+
+    info = Information()
+    info.info_name = info_name
+    info.info_abstract = info_abstract
+    info.info_content = info_content
+    # info.cate1_id = cate1
+    # info.cate2_id = cate2
+    # info.cate3_id = cate3
+    # info.cate4_id = cate4
+    info.upload_user_id = user_id
+    info.upload_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
+    try:
+        info.save()
+    except Exception as e:
+        print(e)
+
+    return HttpResponseRedirect('/info/all_show')
+
+
+# 删除知识
+def delete_info(request):
+    info_id = int(request.GET.get('id', 0))
+
+    Information.objects.filter(id=info_id).delete()
+
+    return HttpResponseRedirect('/info/all_show')
