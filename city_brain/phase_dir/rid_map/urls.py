b4b7e651a538aef33bf6fbd8dcb1f153a61bf53a
barry <lichunbai@outlook.com>
2020-05-06 13:19:53 +0800

    temp

--- city_brain/phase_dir/rid_map/urls.py
+++ city_brain/phase_dir/rid_map/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('rid_map/', views.rid_map_show),
+    path('get_road_info/', views.get_road_info),
+    path('get_rid_info/', views.get_rid_info),
 ]
--- city_brain/phase_dir/rid_map/views.py
+++ city_brain/phase_dir/rid_map/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import CustFroad, InterRid
 from .utils import get_pos2
 
@@ -15,6 +16,42 @@ def rid_map_show(request):
                }
 
     return render(request, 'froad_rid.html', context)
+
+
+# 获取电科路段信息
+def get_road_info(request):
+    road_id = request.POST.get('road_id', '')
+
+    try:
+        road_info = CustFroad.objects.get(id=road_id)
+
+        res = {'angle': road_info.cust_froad_angle,
+               'name': road_info.cust_froad_name,
+               }
+    except:
+        res = {'angle': '',
+               'name': '',
+               }
+
+    return JsonResponse(res)
+
+
+# 获取rid信息
+def get_rid_info(request):
+    rid_id = request.POST.get('rid_id', '')
+
+    try:
+        rid_info = InterRid.objects.get(id=rid_id)
+
+        res = {'angle': rid_info.ft_angle,
+               'name': rid_info.rid_name,
+               }
+    except:
+        res = {'angle': '',
+               'name': '',
+               }
+
+    return JsonResponse(res)
 
 
 
